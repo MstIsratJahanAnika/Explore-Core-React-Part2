05819adfa8574c44699b53d8cd09857a60e4d3e2
MstIsratJahanAnika <mstisratjahananika@gmail.com>
2026-04-19 21:51:24 +0600

    useState to update count

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import './App.css'
 
+import Counter from './Counter';
+
 function App() {
 
-  function handleClick(){
+  function handleClick() {
     alert('I am clicked');
   }
 
   // arrow function, use mostly 
-  const handleClick3 = ()=>{
+  const handleClick3 = () => {
     alert('I am clicked 3rd time');
   }
 
   // parameter pass diye event handling
-  const handleAdd4 = (num)=>{
+  const handleAdd4 = (num) => {
     const newNum = num + 5;
     alert(newNum);
   }
@@ -20,24 +22,28 @@ function App() {
   return (
     <>
       <h1>Get started</h1>
-      {/* <button onClick={handleClick()}>Click Me</button> */}
-      <button onClick={handleClick}>Click Me</button>
 
-      {/* evabe kora jay but korbo na */}
-      <button onClick={function handleClick2(){
-        alert('I am also clicked');
-      }}>Click Me 2nd time</button>
+      <Counter></Counter>
 
-      <button onClick={handleClick3}>Click Me 3rd time</button>
+      <div style={{display: 'flex', gap: '10px', justifyContent: 'center'}}>
+        {/* <button onClick={handleClick()}>Click Me</button> */}
+        <button onClick={handleClick}>Click Me</button>
 
-      {/* arrow function direct ekhane use kora jay, majhe moddhe kora hobe */}
-      <button onClick={()=>{
-        alert('I am clicked for 4th time');
-      }}>Click Me 4th time</button>
+        {/* evabe kora jay but korbo na */}
+        <button onClick={function handleClick2() {
+          alert('I am also clicked');
+        }}>Click Me 2nd time</button>
 
-      {/* parameter pass diye event handling */}
-      <button onClick={()=> handleAdd4(7)}>Click Add 5</button>
+        <button onClick={handleClick3}>Click Me 3rd time</button>
 
+        {/* arrow function direct ekhane use kora jay, majhe moddhe kora hobe */}
+        <button onClick={() => {
+          alert('I am clicked for 4th time');
+        }}>Click Me 4th time</button>
+
+        {/* parameter pass diye event handling */}
+        <button onClick={() => handleAdd4(7)}>Click Add 5</button>
+      </div>
     </>
   )
 }
--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,12 +1,26 @@
+import { useState } from "react"
+
 export default function Counter(){
+    const state = useState; // useState react theke ashbe jokhon variable ei nam e declare kora hobe
+    const [count, setCount] = useState(0); //setState er syntax, variable er nam er shamne set likhe dao, and useState a initial value diye dao  
+
+    // event handling function
+    const handleAdd = ()=>{
+        // console.log('Add button clicked'); //success
+        // setCount(5);  //argument joto pass korbe count er man same hobe 
+
+        // click er shathe shathe count barbe 
+        const newCount = count + 1;
+        setCount(newCount);
+    }
     // css
     const CounterStyle = {
         border: '2px solid red'
     }
     return (
         <div style={CounterStyle}>
-            <h3>Count: </h3>
-            <button>Add</button>
+            <h3>Count: {count}</h3>
+            <button onClick={handleAdd}>Add</button>
         </div>
     )
 }
